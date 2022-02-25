e92372508bd8efb006c7474198fb44526c37de05
wayn <1669738430@qq.com>
2022-02-25 16:36:09 +0800

    refactor(newbee): 代码优化
    1、修复秒杀bug
    2、更新readme
    3、修改样式

--- src/main/java/ltd/newbee/mall/redis/RedisCache.java
+++ src/main/java/ltd/newbee/mall/redis/RedisCache.java
@@ -49,6 +49,9 @@ public class RedisCache {
     public Long luaDecrement(final String key) {
         RedisScript<Long> redisScript = new DefaultRedisScript<>(buildLuaDecrScript(), Long.class);
         Number execute = (Number) redisTemplate.execute(redisScript, Collections.singletonList(key));
+        if (execute == null) {
+            return -1L;
+        }
         return execute.longValue();
     }
 
